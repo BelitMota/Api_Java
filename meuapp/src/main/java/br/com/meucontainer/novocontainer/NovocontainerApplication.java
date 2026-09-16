0273dbbabb51a64332884a7faa2eb394df1c78e3
BelitMota <belitmota@gmail.com>
2026-09-16 20:51:23 -0300

    arquivo atualizado

--- meuapp/src/main/java/br/com/meucontainer/novocontainer/NovocontainerApplication.java
+++ meuapp/src/main/java/br/com/meucontainer/novocontainer/NovocontainerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NovocontainerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MeuappApplication.class, args);
+		SpringApplication.run(NovocontainerApplication.class, args);
 	}
 
 }
