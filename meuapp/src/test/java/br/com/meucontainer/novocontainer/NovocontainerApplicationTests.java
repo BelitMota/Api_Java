33f6afd3bc353486ad5be81a1163da14aefc3de0
BelitMota <belitmota@gmail.com>
2026-09-16 19:27:43 -0300

    arquivo atualizado

--- meuapp/src/test/java/br/com/meucontainer/novocontainer/NovocontainerApplicationTests.java
+++ meuapp/src/test/java/br/com/meucontainer/novocontainer/NovocontainerApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.meugrupo.meuapp;
+package br.com.meucontainer.novocontainer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MeuappApplicationTests {
+class NovocontainerApplicationTests {
 
 	@Test
 	void contextLoads() {
