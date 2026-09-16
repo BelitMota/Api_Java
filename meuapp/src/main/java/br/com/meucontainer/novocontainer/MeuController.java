1060123751da77564d7bc613d3896e92a319ada5
BelitMota <belitmota@gmail.com>
2026-09-16 20:21:19 -0300

    arquivo atualizado

--- meuapp/src/main/java/br/com/meucontainer/novocontainer/MeuController.java
+++ meuapp/src/main/java/br/com/meucontainer/novocontainer/MeuController.java
@@ -1,15 +1,17 @@
-package br.com.meucontainer.novocontainer;
+package br.com.meugrupo.meuapp; // define o pacote ao qual a classe pertence
 
+// importa a anotação GetMapping (definição do endpoint que responde a
+// uma requisição HTTP GET) 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// importa a anotação RestController (informa ao SB que a classe será
+// responsável por atender requisições HTTP e retornar dados como resposta)
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class MeuController {
-
-    @GetMapping("/demo")
-    public String mensagem() {
-        return "<h1>Demonstração do curso</h1><p>API funcionando corretamente!</p><strong>Status: ONLINE</strong>";
+    @GetMapping("/api") // informa caminho
+    public String mensagem() { // método mensagem() que retorna uma string
+        System.out.println("Resposta da API REST (println).");
+        return "Resposta da API REST (retorno).";
     }
 }
--- meuapp/src/main/java/br/com/meucontainer/novocontainer/NovocontainerApplication.java
+++ meuapp/src/main/java/br/com/meucontainer/novocontainer/NovocontainerApplication.java
@@ -1,13 +1,15 @@
-package br.com.meucontainer.novocontainer;
+package br.com.meugrupo.meuapp; // define o pacote ao qual a classe pertence
 
+// importa a classe que possui o método que permite iniciar uma app SB
 import org.springframework.boot.SpringApplication;
+// importa a anotação que reune várias configurações do SB
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NovocontainerApplication {
+public class MeuappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NovocontainerApplication.class, args);
+		SpringApplication.run(MeuappApplication.class, args);
 	}
 
 }
